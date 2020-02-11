51f35d16701fc54bca7b7497bebeb19520702fb5
Lars Kristian Maron Telle <larskristian.marontelle@gmail.com>
2020-02-11 11:37:22 +0100

    Create Markov chain of example text

--- src/markov_elear/generator.clj
+++ src/markov_elear/generator.clj
@@ -1,4 +1,16 @@
-(ns markov-elear.generator)
+(ns markov-elear.generator
+  (:require [clojure.string :as streng]
+            [clojure.set :as mengde]))
+
+(def example "And the Golden Grouse And the Pobble who")
+(def words (streng/split example #" "))
+(def word-transitions (partition-all 3 1 words))
+(def markov-chain (reduce (fn [r t]
+                            (merge-with mengde/union r
+                                        (let [[a b c] t]
+                                          {[a b] (if c #{c} #{})})))
+                          {}
+                          word-transitions))
 
 (defn foo
   "I don't do a whole lot."
