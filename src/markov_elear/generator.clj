a517195fb2008043480ee499ec58ef8c415432bc
Lars Kristian Maron Telle <larskristian.marontelle@gmail.com>
2020-02-11 13:07:56 +0100

    Add test for markov-chain

--- src/markov_elear/generator.clj
+++ src/markov_elear/generator.clj
@@ -5,12 +5,14 @@
 (def example "And the Golden Grouse And the Pobble who")
 (def words (streng/split example #" "))
 (def word-transitions (partition-all 3 1 words))
-(def markov-chain (reduce (fn [r t]
-                            (merge-with mengde/union r
-                                        (let [[a b c] t]
-                                          {[a b] (if c #{c} #{})})))
-                          {}
-                          word-transitions))
+(defn markov-chain
+  [word-transitions]
+  (reduce (fn [r t]
+            (merge-with mengde/union r
+                        (let [[a b c] t]
+                          {[a b] (if c #{c} #{})})))
+          {}
+          word-transitions))
 
 (defn foo
   "I don't do a whole lot."
